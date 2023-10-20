4d1b55a4adfd4c20d440b2045c8226300c87df9e
jwpark2023 <139726025+jwpark2023@users.noreply.github.com>
2023-10-20 19:47:44 +0900

    nextjs env 변수명은 "NEXT_PUBLIC_" 꼭 붙여야 인식됨

--- src/client/base.ts
+++ src/client/base.ts
@@ -1,33 +1,28 @@
 import ky from "ky-universal";
 
 export const fetcher = (input: URL | RequestInfo, init?: RequestInit | undefined) =>
-  ky(`${process.env.RESOURCE_API_ENDPOINT}/${input}`, init).then((res) => res.json());
+    ky(`${process.env.RESOURCE_API_ENDPOINT}/${input}`, init).then((res) => res.json());
 
 export const fetchApi = ky.create({
-  prefixUrl: process.env.RESOURCE_COMMON_API_ENDPOINT,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    prefixUrl: process.env.NEXT_PUBLIC_API_ENDPOINT,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 export const fetchResourceApi = {
-  commomApi: ( ky.create({
-        prefixUrl: process.env.RESOURCE_COMMON_API_ENDPOINT,
+    commomApi: ky.create({
+        prefixUrl: process.env.NEXT_PUBLIC_COMMON_API_ENDPOINT,
         timeout: 10000,
         throwHttpErrors: false,
         headers: {
-          "Content-Type": "application/json",
+            "Content-Type": "application/json",
         },
-      })), 
-  ghgApi: ( ky.create({
-        prefixUrl: process.env.RESOURCE_GHG_API_ENDPOINT,
+    }),
+    ghgApi: ky.create({
+        prefixUrl: process.env.NEXT_PUBLIC_GHG_API_ENDPOINT,
         headers: {
-          "Content-Type": "application/json",
+            "Content-Type": "application/json",
         },
-      })), 
+    }),
 };
-
-
-
-
-
